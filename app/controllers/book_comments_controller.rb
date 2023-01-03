3f2161418feeb6093d36f1342e307f5ef6d34359
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-01-03 15:26:08 +0000

    コメント非同期通信投稿エラー

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,19 +5,16 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  render 'books/show'
+		unless @book_comment.save
 		end
-
+        
  end
 
  def destroy
    	@book = Book.find(params[:book_id])
-  	book_comment = @book.book_comments.find(params[:id])
-		book_comment.destroy
-		redirect_to request.referer
+  	@book_comment = @book.book_comments.find(params[:id])
+	@book_comment.destroy
+	
  end
 
   private
