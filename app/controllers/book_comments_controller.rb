f3d7e00db0399905108cd827e08b586710afada9
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-01-04 04:47:56 +0000

    応用課題６機能OK

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,23 @@
 class BookCommentsController < ApplicationController
  before_action :authenticate_user!
  def create
-   	@book = Book.find(params[:book_id])
+  @book = Book.find(params[:book_id])
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		unless @book_comment.save
+	 if @book_comment.save
+	  redirect_to book_path(@book.id)
+		else
+		  render 'books/show'
 		end
-        
  end
+        
 
  def destroy
    	@book = Book.find(params[:book_id])
   	@book_comment = @book.book_comments.find(params[:id])
-	@book_comment.destroy
+	  @book_comment.destroy
+	  redirect_to request.referer
 	
  end
 
