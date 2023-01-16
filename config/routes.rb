97e4f7159df218a847bca93055c42ffe275ce2ad
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-01-16 07:27:21 +0000

    ゲストログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,10 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
    end
- 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
